b0fe05d602dea11235ba2bf6cc15447d9b584484
IldarGH02 <kooooooffe@gmail.com>
2026-06-03 12:18:51 +0400

    ix: remove duplicate exports from eports.tsx

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,8 +1,8 @@
-export * from './Reports/Reports';
-export * from './Login/Login';
-export * from './Dashboard/Dashboard';
-export * from './Trips/Trips';
-export * from './Settings/Settings';
-export * from './FuelCalculator/FuelCalculator';
-export * from './Admin/Admin';
-export * from './Privacy/Privacy';
+export { Reports } from './Reports/Reports';
+export { Login } from './Login/Login';
+export { Dashboard } from './Dashboard/Dashboard';
+export { Trips } from './Trips/Trips';
+export { Settings } from './Settings/Settings';
+export { FuelCalculator } from './FuelCalculator/FuelCalculator';
+export { Admin } from './Admin/Admin';
+export { Privacy } from './Privacy/Privacy';
